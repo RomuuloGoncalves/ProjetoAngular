32be5a75a9c530e3a6d5a4ca91e530c67cf85cdc
Rômulo Gonçalves <104324780+RomuuloGoncalves@users.noreply.github.com>
2023-06-15 11:12:26 +0000

    #

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,10 +8,10 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, RouterModule],
 })
 export class HomePage {
-  constructor() {}perfil = {
+  perfil = {
     nome: 'Carlitos', 
     profissao: 'Professor', 
     mencao: '@carlitos', 
