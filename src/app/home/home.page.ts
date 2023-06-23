bcc0f5a132c8c2cd7ebfbafad5cd1b4f99abd474
sousa-p <p.soousa7@gmail.com>
2023-06-23 18:41:21 -0300

    Feature calculos e histórico calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
-
 export class HomePage {
-
   constructor() {}
-  
-  operacao: number = 432435435456465264525465656256256245;
+
+  historico: Array<string> = [];
+  operacao: string = '';
   resultado: number = 0;
+
+  addOperacao(char: string) {
+    this.operacao += char;
+    this.preResultado();
+  }
+
+  mudarOperacao(operacao: string) {
+    this.operacao = operacao;
+    this.preResultado()
+  }
+  preResultado() {
+    this.resultado = eval(this.operacao);
+  }
+
+  realizarOperacao() {
+    if (this.operacao != '') {
+      this.resultado = eval(this.operacao);
+      this.historico.push(this.operacao);
+      this.operacao = String(this.resultado);
+    }
+  }
+
+  limparOperacao() {
+    this.operacao = '';
+    this.resultado = 0;
+  }
+
+  limparCharOperacao() {
+    this.operacao = this.operacao.slice(0, -1);
+    this.preResultado();
+  }
+
+  salvarHistorico() {
+    this.historico.push(this.operacao);
+  }
 }
