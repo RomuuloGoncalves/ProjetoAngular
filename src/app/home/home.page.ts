f1ea54bd9bca44b3112b2a0259d72d7c2b348769
sousa-p <p.soousa7@gmail.com>
2023-06-24 17:19:56 -0300

    Feature validação de entrada de dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,51 +13,88 @@ export class HomePage {
   constructor() {}
 
   historicoOperacoes: Array<any> = [];
-  caracteresEspeciais: Array<string> = ['+', '-', '/', '%', '*', '.'];
   operacao: string = '';
   resultado: number = 0;
 
-  addOperacao(char: string) {
-    const ultimoCaracterEhEspecial = this.caracteresEspeciais.includes(this.operacao.slice(-1));
-    const novoCaracterEhEspecial = this.caracteresEspeciais.includes(char);
-
-    // Realizar Validação
-    if (true) {
+  addOperacao(char: string): void {
+    if (this.operacaoEhValida(this.operacao, char)) {
       this.operacao += char;
-      this.preResultado();
+      this.calcResultado();
     }
   }
 
-  mudarOperacao(operacao: string) {
-    this.operacao = operacao;
-    this.preResultado();
-  }
-  preResultado() {
-    this.resultado = eval(this.operacao);
+  operacaoEhValida(
+    operacao: string = this.operacao,
+    char: string = ''
+  ): boolean {
+    const ultimoCharOperacaoEhNumber = !isNaN(Number(operacao.slice(-1)));
+    const regexOperacoes = /[+%./(*-]/;
+
+    if (operacao === '' && char === '-') {
+      return true;
+    }
+
+    if (operacao === '' && regexOperacoes.test(char)) {
+      return false;
+    }
+
+    if (ultimoCharOperacaoEhNumber && regexOperacoes.test(char)) {
+      return true;
+    }
+
+    try {
+      const aux = eval(operacao + char);
+      return (
+        typeof aux === 'number' && Number.isFinite(aux) && !Number.isNaN(aux)
+      );
+    } catch (_) {
+      return false;
+    }
   }
 
-  realizarOperacao() {
-    if (this.operacao != '') {
-      this.resultado = eval(this.operacao);
-      this.historicoOperacoes.push({
-        operacao: this.operacao,
-        resultado: this.resultado,
-      });
+  mudarOperacao(novaOperacao: string): void {
+    this.operacao = novaOperacao;
+    this.calcResultado();
+  }
+
+  inverterSinal(): void {
+    this.finalizaOperacao();
+    this.resultado = this.resultado * -1;
+    this.operacao = this.resultado.toLocaleString();
+  }
+
+  calcResultado(operacao: string = this.operacao): void {
+    try {
+      operacao ? (this.resultado = eval(operacao)) : (this.resultado = 0);
+    } catch (_) {
+      this.resultado = 0;
+    }
+  }
+
+  finalizaOperacao(): void {
+    if (isNaN(Number(this.operacao.slice(0, -1)))) {
+      this.calcResultado();
+      this.salvarHistorico(this.operacao, this.resultado);
       this.operacao = String(this.resultado);
     }
   }
 
-  limparOperacao() {
+  limparOperacao(): void {
     this.operacao = '';
     this.resultado = 0;
   }
 
-  limparCharOperacao() {
+  limparCharOperacao(): void {
     this.operacao = this.operacao.slice(0, -1);
-    this.preResultado();
+    this.calcResultado();
   }
 
-  salvarHistorico() {
-    this.historicoOperacoes.push(this.operacao);
+  salvarHistorico(operacao: string, resultado: number): void {
+    if (operacao.slice(0, -1) != resultado.toString()) {
+      this.historicoOperacoes.push({
+        operacao: operacao,
+        resultado: resultado,
+      });
+    }
   }
 }
