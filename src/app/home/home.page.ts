f51c653a69b0b12a9b158b89aefa8e45a84b2fe2
Pedro S <97417230+sousa-p@users.noreply.github.com>
2023-06-27 13:52:16 +0000

    #

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,6 +19,9 @@ export class HomePage {
   addOperacao(char: string): void {
     if (this.operacaoEhValida(this.operacao, char)) {
       this.operacao += char;
+      if (char === '%') {
+        this.calcPorcentagem();
+      }
       this.calcResultado();
     }
   }
@@ -27,31 +30,29 @@ export class HomePage {
     operacao: string = this.operacao,
     char: string = ''
   ): boolean {
-    const ultimoChar = operacao.slice(-1);
-    const ultimoCharOperacaoEhNumber = !isNaN(Number(ultimoChar));
-    const regexOperacoes = /[+%./(*-]/;
+    const ultimoChar: any = operacao?.at(-1);
+    const regexCharsEspeciais = /[+%./*-]/;
     const regexPorcentagemBlock = /[%.]/;
-
     if (operacao === '' && char === '-') {
       return true;
     }
-    if (operacao === '' && regexOperacoes.test(char)) {
+    if (operacao === '' && regexCharsEspeciais.test(char)) {
       return false;
     }
-    if (ultimoCharOperacaoEhNumber && regexOperacoes.test(char)) {
+    if (!regexCharsEspeciais.test(ultimoChar) && regexCharsEspeciais.test(char)) {
       return true;
     }
-
     if (
       ultimoChar === '%' &&
       !regexPorcentagemBlock.test(char) &&
-      regexOperacoes.test(char)
+      regexCharsEspeciais.test(char)
     ) {
       return true;
     }
-
+    console.log(5)
     try {
       const aux = eval(operacao + char);
+      
       return (
         typeof aux === 'number' && Number.isFinite(aux) && !Number.isNaN(aux)
       );
@@ -71,20 +72,17 @@ export class HomePage {
     this.operacao = this.resultado.toLocaleString();
   }
 
-  calcPorcentagem(operacao: string): string {
-    const result = operacao.replace(/(\d+[+./-])+\d+%/g, (m: string) => {
-      const regexDesmembrarPorcentagem = /\d+|%|[+\-*/]/g;
-
-      const arr: any = m.match(regexDesmembrarPorcentagem);
-      return `${arr[0]}${arr[1]}(${arr[0]}*${arr[2]}/100)`;
-    });
-
-    console.log(result);
-    return result;
+  calcPorcentagem(): void {
+    const numeros = this.operacao.match(/\d*/);
+    const operacaoSemPorcentagem = this.operacao.split(/\d+%/)[0];
+    const porcentagem = Number(numeros?.at(-1)) / 100;
+    const penultimoNumero = numeros?.at(-2);
+    const resultado = eval(`${penultimoNumero}*${porcentagem}`);
+    console.log(operacaoSemPorcentagem, porcentagem, penultimoNumero, resultado)
+    this.operacao = operacaoSemPorcentagem + resultado.toString();
   }
 
   calcResultado(operacao: string = this.operacao): void {
-    operacao = this.calcPorcentagem(operacao);
     try {
       operacao ? (this.resultado = eval(operacao)) : (this.resultado = 0);
     } catch (_) {
